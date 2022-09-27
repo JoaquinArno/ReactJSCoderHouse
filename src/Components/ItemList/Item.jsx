b5d4e59500b029c9f38a2504b2e3ce131f8dc818
Joaquin Arno <arnojoaquin17@gmail.com>
2022-09-27 19:30:07 -0300

    ReadMe

--- src/Components/ItemList/Item.jsx
+++ src/Components/ItemList/Item.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import {Link} from "react-router-dom";
+import "./Item.css"
 
 
 function Item({ product }) {
 
     return (
 
-        <Card style={{ width: '14rem' }}>
-            <Card.Img variant="top" src={product.img} />
-            <Card.Body>
-                <Card.Title>"{product.titulo}"</Card.Title>
-                <Card.Text>{product.autor}</Card.Text>
-                <Link to={`/producto/${product.id}`} ><Button variant="primary">Ver más</Button></Link>  
-            </Card.Body>
-        </Card>
+            <Card className="card" style={{ width: '12rem' }}>
+                <Card.Img variant="top" src={product.img} />
+                <Card.Body>
+                    <Card.Title>"{product.titulo}"</Card.Title>
+                    <Card.Text>{product.autor}</Card.Text>
+                    <Link to={`/producto/${product.id}`} ><Button variant="primary">Ver más</Button></Link>
+                </Card.Body>
+            </Card>
     );
 }
 export default Item
